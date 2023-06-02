77f16a877596bbbf6feeb88e06c15bf654583252
RW224101 <135328734+RW224101@users.noreply.github.com>
2023-06-02 20:28:22 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+const cookieBox = document.querySelector(".wrapper"),
+    buttons = document.querySelectorAll(".button"),
+
+const executeCodes = () => {
+    cookieBox.classList.add("show")
+};
+
+window.addEventListener("load", executeCodes)
